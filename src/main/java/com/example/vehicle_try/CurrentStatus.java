97d0d11a1a0c28c52c2e721ed71fba9c7f775924
karan262 <mehtakaran000@gmail.com>
2021-02-14 10:52:21 +0530

    New

---
+++ src/main/java/com/example/vehicle_try/CurrentStatus.java
@@ -0,0 +1,14 @@
+package com.example.vehicle_try;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_status);
+    }
+}
