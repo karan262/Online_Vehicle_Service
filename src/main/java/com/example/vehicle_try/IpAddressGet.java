d712e9fd61cbd5b2f622ac86185daf37280615b3
karan262 <mehtakaran000@gmail.com>
2021-04-08 01:06:57 +0530

    pvu

--- src/main/java/com/example/vehicle_try/IpAddressGet.java
+++ src/main/java/com/example/vehicle_try/IpAddressGet.java
@@ -1,7 +1,7 @@
 package com.example.vehicle_try;
 
 public class IpAddressGet {
-    static String ip = "http://192.168.1.2/VehicleBook/";
+    static String ip = "http://192.168.1.3/VehicleBook/";
 
     public static String getIp() {
         return ip;
